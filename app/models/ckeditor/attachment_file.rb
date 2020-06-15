8b8585ff9096629a0a5fd6f5ea9bd22cad2bf895
Denis Gavrilin <den.gavrilin1@gmail.com>
2020-06-15 12:24:00 +0300

    added default config for active storage

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Ckeditor::AttachmentFile < Ckeditor::Asset
-  has_one_attached :data
   validates :data, attached: true, size: { less_than: 100.megabytes , message: 'is not given between size' }
 
   def url_thumb
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Ckeditor::Picture < Ckeditor::Asset
-  has_one_attached :data
   validates :data, attached: true, content_type: /\Aimage/, size: { less_than: 2.megabytes , message: 'is not given between size' }
+
+  def url_content
+    rails_representation_url(storage_data.variant(resize: '800>').processed, only_path: true)
+  end
+
+  def url_thumb
+    rails_representation_url(storage_data.variant(resize: '118x100').processed, only_path: true)
+  end
 end
